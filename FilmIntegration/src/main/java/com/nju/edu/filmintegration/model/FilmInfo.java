744ddc71d8d8279dea736ceb874dc0b73c405397
Peng <610137708@qq.com>
2019-05-19 16:33:10 +0800

    电影票信息页面
    电影票信息页面大体完成

---
+++ FilmIntegration/src/main/java/com/nju/edu/filmintegration/model/FilmInfo.java
@@ -0,0 +1,75 @@
+package com.nju.edu.filmintegration.model;
+
+import java.util.Date;
+
+/**
+ * @Auther: peng
+ * @Date: 2019/5/19 13:37
+ * @Description:
+ */
+public class FilmInfo {
+
+    private String name;
+    private String cinema;
+    private String date;
+    private String auditorium;
+    private String time;
+    private String price;
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAuditorium() {
+        return auditorium;
+    }
+
+    public String getCinema() {
+        return cinema;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setAuditorium(String auditorium) {
+        this.auditorium = auditorium;
+    }
+
+    public void setCinema(String cinema) {
+        this.cinema = cinema;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
--- FilmIntegration/src/main/resources/static/js/single.js
+++ FilmIntegration/src/main/resources/static/js/single.js
@@ -1,1 +1,80 @@
-$("#mytab").tab('show');
+$(function(){
+
+    $("#mytab").tab('show');
+
+    var targetMovie = "大侦探皮卡丘";
+
+
+    $("#nuomi_table").bootstrapTable({
+        url:'/api/filmInfo/nuomi',         //请求后台的URL（*）
+        method:'get',
+        striped: true,                      //是否显示行间隔色
+        cache: false,
+        pagination: true,                   //是否显示分页（*）
+        sidePagination: "client",           //分页方式
+        pageNumber: 1,                      //初始化加载第一页，默认第一页,并记录
+        pageSize: 10,                     //每页的记录行数（*）
+        search: true,                      //是否显示表格搜索
+        showToggle: true,                   //卡片、表格切换按钮
+        locale: "zh-CN",
+        queryParams:{
+            name:targetMovie
+        },
+        columns: [{
+            field: 'name',
+            title: '电影名称'
+        }, {
+            field: 'cinema',
+            title: '电影院'
+        }, {
+            field: 'date',
+            title: '日期'
+        }, {
+            field: 'auditorium',
+            title: '厅'
+        }, {
+            field: 'time',
+            title: '时间'
+        }, {
+            field: 'price',
+            title: '票价'
+        }]
+    });
+
+
+    $("#meituan_table").bootstrapTable({
+        url:'/api/filmInfo/meituan',         //请求后台的URL（*）
+        method:'get',
+        striped: true,                      //是否显示行间隔色
+        cache: false,
+        pagination: true,                   //是否显示分页（*）
+        sidePagination: "client",           //分页方式
+        pageNumber: 1,                      //初始化加载第一页，默认第一页,并记录
+        pageSize: 10,                     //每页的记录行数（*）
+        search: true,                      //是否显示表格搜索
+        showToggle: true,                   //卡片、表格切换按钮
+        locale: "zh-CN",
+        queryParams:{
+            name:targetMovie
+        },
+        columns: [{
+            field: 'name',
+            title: '电影名称'
+        }, {
+            field: 'cinema',
+            title: '电影院'
+        }, {
+            field: 'date',
+            title: '日期'
+        }, {
+            field: 'auditorium',
+            title: '厅'
+        }, {
+            field: 'time',
+            title: '时间'
+        }, {
+            field: 'type',
+            title: '种类'
+        }]
+    });
+});
